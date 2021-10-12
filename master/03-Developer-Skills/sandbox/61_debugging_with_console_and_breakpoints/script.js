b517b99754cbc8b0d572d45d4efe7eded326123f
timfilipowski88 <timfilipowski88@gmail.com>
2021-10-12 09:30:34 -0500

    update

--- master/03-Developer-Skills/sandbox/61_debugging_with_console_and_breakpoints/script.js
+++ master/03-Developer-Skills/sandbox/61_debugging_with_console_and_breakpoints/script.js
@@ -1,3 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius:')),
+  };
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  console.log(measurement);
+  console.table(measurement);
+
+  // debugger;
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
