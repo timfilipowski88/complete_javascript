b1f1d7d8307afeee25642203de29832d18ecc90d
timfilipowski88 <timfilipowski88@gmail.com>
2021-10-27 07:54:55 -0500

    update

--- master/06-Modal/starter/script.js
+++ master/06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('btn clicked');
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
